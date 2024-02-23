c31f4fdaa2229b6f3a3f59dae19ea40a2089149a
Paulo Adson <paulo.alves200581@gmail.com>
2024-02-22 23:17:15 -0300

    aprendendo mais sobre dados

---
+++ inicio/dados_4.js
@@ -0,0 +1,15 @@
+// let a = 3;
+
+
+//// Eu consigo modificar uma variável durante o código
+
+// a = 10 //// antes valia 3 e agora vale 10
+
+// console.log(a) ;
+
+
+////Agora se eu quero uma variável constante eu uso o const
+
+const a = 3
+
+//// Não é possível modificar uma constante no decorrer do código.
